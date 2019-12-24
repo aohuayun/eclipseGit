266c06eca561a4d97c988e55afed2f474332ca43
aohuayun <aohuayun@163.com>
2019-12-24 09:00:09 +0800

    User 修改

--- src/com/imti/entity/User.java
+++ src/com/imti/entity/User.java
@@ -1,5 +1,15 @@
 package com.imti.entity;
 
 public class User {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
